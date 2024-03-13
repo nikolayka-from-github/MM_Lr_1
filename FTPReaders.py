b3cc2ee679a71a3ae04e71cf5659f951f2ba0367
NATikhomirov <natikhomirov@mai.education>
2024-03-14 00:32:05 +0300

    вроде промежуточно готов RinexReader

--- FTPReaders.py
+++ FTPReaders.py
@@ -1,6 +1,7 @@
-import re
+# import re
 import wget
 import os.path
+import math
 
 
 class FTPReader:
@@ -50,28 +51,23 @@ class SP3Reader(FTPReader):
         with open(name) as file:
             str_id = ""
             temp_curr_time = 0.0
-
             while line := file.readline():
                 line = line.split()
-
                 if line[0][0] == 'P':
                     temp_arr_ = []
                     line = line[1:5]
                     for i in range(0, 4):
                         temp_arr_.append(float(line[i]))
                     self.__All_dict[temp_curr_time].append(temp_arr_)
-
                 elif line[0] == '*':
                     temp_curr_time = (int(line[3]) - self.__d_0) * 24 + int(line[4]) + int(line[5]) / 60
                     self.__All_dict[temp_curr_time] = []
                     self.__Arr_Time.append(temp_curr_time)
-
                 elif line[0] == '+':
                     if len(line) == 3:
                         str_id += line[2]
                     else:
                         str_id += line[1]
-
                 elif len(line[0]) == 7:
                     self.__y_0 = int(line[0][3:7])
                     self.__M_0 = int(line[1])
@@ -92,25 +88,111 @@ class SP3Reader(FTPReader):
         temp_id.append(temp_str)
 
         self.__Numb_Sat = len(temp_id)
-        # print(self.__Numb_Sat)
-        # print(temp_id)
         self.__All_dict["Numb_Sat"] = self.__Numb_Sat
         self.__All_dict["Id_Sat"] = temp_id
         self.__All_dict["Arr_Time"] = self.__Arr_Time
-        print(self.__All_dict["Arr_Time"])
 
         return self.__All_dict
 
 
-class RinexReader(FTPReader):
+class RinexReader(FTPReader):  # Only for GNSS!!!
+
+    __y_0 = 0
+    __M_0 = 0
+    __d_0 = 0
+    __h_0 = 0
+    __m_0 = 0
+    __s_0 = 0.
 
     def __init__(self, url, user_name="anonymous", user_pass=""):
         super().__init__(url, user_name, user_pass)
 
+    @staticmethod
+    def split_to_time(line):
+        s = 0
+        y = int(line[1])
+        m_ = int(line[2])
+        d = int(line[3])
+        h = int(line[4])
+        m = int(line[5])
+        if len(line[6]) > 2:
+            if line[6][1] == '-':
+                s = float(line[6][0])
+            else:
+                s = float(line[6][0:2])
+        elif len(line[6]) == 1:
+            s = float(line[6])
+
+        return y, m_, d, h, m, s
+
+    @staticmethod
+    def split_to_exp(line):
+        for i in range(0, 4):
+            v_len = len(line[i])
+            pow_exp = int(line[i][v_len - 2:v_len])
+            sign_pow_exp = -1
+            if line[i][v_len - 3] == "+":
+                sign_pow_exp = 1
+            value__exp = float(line[i][0:v_len - 4])
+            line[i] = value__exp*pow(10, sign_pow_exp*pow_exp)
+        return line
+
+    @staticmethod
+    def split_to_f_value(line):
+        temp_line = ""
+        arr_line = []
+        temp_srt_value = ""
+        for i in line:
+            temp_line += i
+        counter = 0
+        bool_d = False
+        for i in temp_line:
+            temp_srt_value += i
+            if i == "D":
+                bool_d = True
+                continue
+            if bool_d:
+                counter += 1
+            if counter == 3:
+                arr_line.append(temp_srt_value)
+                temp_srt_value = ""
+                counter = 0
+                bool_d = False
+        return arr_line
+
     def Read(self, path, name):
-        self._link = self._link + path + "/" + name
-        if not os.path.exists(name):
-            wget.download(self._link)
-        else:
-            print("Working with a file <...>")
+        super().Read(path, name)
+        with open(name) as file:
+            str_id = ""
+            temp_curr_time = []
+            counter_broad = 1
+            flag_broad = False
+            init_time = False
+            while line := file.readline():
+                line = line.split()
+                if line[0][0] == 'R':
+                    flag_broad = True
+                    if init_time:
+                        init_time = False
+                        [self.__y_0, self.__M_0, self.__d_0,
+                         self.__h_0, self.__m_0,  self.__s_0] = self.split_to_time(line)
+                    else:
+                        line = self.split_to_time(line)
+                elif flag_broad:
+                    counter_broad += 1
+                    if len(line) == 4:
+                        line = self.split_to_exp(line)
+                    else:
+                        temp_line = self.split_to_f_value(line)
+                        line = self.split_to_exp(temp_line)
+                    if counter_broad == 4:
+                        flag_broad = False
+
+                print(line)
+        print(self.__y_0)
+        print(self.__M_0)
+        print(self.__d_0)
+        print(self.__h_0)
+        print(self.__m_0)
+        print(self.__s_0)
         return 1
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-from FTPReaders import SP3Reader
+# from FTPReaders import SP3Reader
 from FTPReaders import RinexReader
 
 Url = "ftp://ftp.glonass-iac.ru"
@@ -9,12 +8,11 @@ File_name = "Sta22913.sp3"
 Path_r = "MCC/BRDC/2023"
 File_name_r = "Brdc0420.23g"
 
-sp3 = SP3Reader(url=Url)
-data = sp3.Read(Path, File_name)
+# sp3 = SP3Reader(url=Url)
+# data = sp3.Read(Path, File_name)
 # print(data)
 
-# rin = RinexReader(url=Url)
-# rin.Read(Path_r, File_name_r)
+rin = RinexReader(url=Url)
+rin.Read(Path_r, File_name_r)
 
 # Test arr:
-
