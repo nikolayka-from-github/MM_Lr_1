24dd382d10f5e81fb87e092e576064cf939e8822
NATikhomirov <natikhomirov@mai.education>
2024-03-13 17:10:12 +0300

    add Rinex and SP3 skeleton readers

--- FTPReaders.py
+++ FTPReaders.py
@@ -1,15 +1,58 @@
 from ftplib import FTP
 import re
 import wget
+import os.path
 
 
 class FTPReader:
     _url = ""
     _name = ""
+    _user_name = ""
+    _user_pass = ""
+    _link = ""
 
     def __init__(self, url, user_name, user_pass):
-        self._ftp = FTP(url, encoding='cp1251')
-        self._ftp.login(user=user_name, passwd=user_pass)
+        self._url = url
+        self._user_name = user_name
+        self._user_pass = user_pass
+        self._link = url + "/"
 
-    def Read(self, name):
+    def Read(self, path, name):
         return NotImplementedError(f"In the <{self.__name__}> class, the method must be redefined <Read>")
+
+    @property
+    def url(self):
+        return self._url
+
+    @url.setter
+    def url(self, url):
+        self._url = url
+
+
+class SP3Reader(FTPReader):
+
+    def __init__(self, url, user_name="anonymous", user_pass=""):
+        super().__init__(url, user_name, user_pass)
+
+    def Read(self, path, name):
+        self._link = self._link + path + "/" + name
+        if not os.path.exists(name):
+            wget.download(self._link)
+        else:
+            print("Working with a file <...>")
+        return 1
+
+
+class RinexReader(FTPReader):
+
+    def __init__(self, url, user_name="anonymous", user_pass=""):
+        super().__init__(url, user_name, user_pass)
+
+    def Read(self, path, name):
+        self._link = self._link + path + "/" + name
+        print(self._link)
+        if not os.path.exists(name):
+            wget.download(self._link)
+        else:
+            print("Working with a file <...>")
+        return 1
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import math
+from FTPReaders import SP3Reader
+from FTPReaders import RinexReader
+
+Url = "ftp://ftp.glonass-iac.ru"
+Path = "MCC/PRODUCTS/23340/final"
+File_name = "Sta22913.sp3"
+
+Path_r = "MCC/BRDC/2023"
+File_name_r = "Brdc0420.23g"
+
+sp3 = SP3Reader(url=Url)
+sp3.Read(Path, File_name)
+
+rin = RinexReader(url=Url)
+rin.Read(Path_r, File_name_r)
